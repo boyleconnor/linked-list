30da166cdbeb41c964849e71425fee60c81358ba
Connor Boyle <connor.bo@gmail.com>
2023-11-18 16:08:20 -0800

    Implement iter for third version

--- src/third.rs
+++ src/third.rs
@@ -4,6 +4,13 @@ pub struct List<T> {
     head: Link<T>
 }
 
+type Link<T> = Option<Rc<Node<T>>>;
+
+pub struct Node<T> {
+    element: T,
+    next: Link<T>
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -25,13 +32,24 @@ impl<T> List<T> {
             )
         }
     }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() }
+    }
 }
 
-type Link<T> = Option<Rc<Node<T>>>;
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
 
-pub struct Node<T> {
-    element: T,
-    next: Link<T>
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.element
+        })
+    }
 }
 
 #[cfg(test)]
@@ -62,6 +80,17 @@ mod test {
         assert_eq!(x.head.clone().unwrap().element, 5);
         x = x.tail();
         assert!(x.head.is_none());
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(3).prepend(5).prepend(11);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&11));
+        assert_eq!(iter.next(), Some(&5));
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
     }
 }
 
